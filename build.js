13f14f9f41bfa40794573063505e333b48227509
Chris Yang <yy4382@outlook.com>
2025-02-11 23:32:26 +0800

    Add initial project setup with package.json and build script

--- build.js
+++ build.js
@@ -1,41 +1,23 @@
-const { exec } = await import("node:child_process");
-const esbuild = await import("esbuild");
+import * as esbuild from "esbuild";
+import clipboardy from "clipboardy";
+import { readFileSync } from "fs";
 
-function copyToClipboard(text) {
-  const command =
-    process.platform === "darwin"
-      ? `echo "${text}" | pbcopy` // macOS
-      : process.platform === "win32"
-      ? `echo ${text} | clip` // Windows
-      : `echo "${text}" | xclip -selection clipboard`; // Linux with xclip
+async function minifyCode(inputFile) {
+  const minifiedCode = (
+    await esbuild.transform(readFileSync(inputFile, "utf8"), {
+      minify: true,
+    })
+  ).code;
 
-  exec(command, (error) => {
-    if (error) {
-      console.error("复制失败:", error);
-      return;
-    }
-    console.log("内容已复制到剪贴板");
-  });
-}
-async function minifyCode(code) {
-  const result = await esbuild.transform(code, {
-    minify: true,
-  });
-
-  return result.code;
+  // for unknown reasons, if minified, the \n in string will be real line break
+  return minifiedCode.slice(0, -1).replaceAll("\n", "\\n");
 }
 
-async function build(code) {
-  const minified = await minifyCode(code);
-  copyToClipboard(minified);
+async function build() {
+  const minified = await minifyCode("search-engine-switch.js");
+  const bookmarklet = `javascript:(function(){${minified}})()`;
+  clipboardy.writeSync(bookmarklet);
+  console.log("Bookmarklet copied to clipboard");
 }
 
-async function getCode(
-  url = "https://gist.githubusercontent.com/yy4382/dfd17cdadb5c6c61bafbd277869fd713/raw/search-engine-switch.js"
-) {
-  const response = await fetch(url);
-  const code = await response.text();
-  return code;
-}
-
-build(await getCode());
+build();
