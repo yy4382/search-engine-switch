83c63197e427537270b0bc3d2e8374b917608cd0
Chris Yang <yy4382@outlook.com>
2025-02-11 23:39:14 +0800

    rewrite

--- build.js
+++ build.js
@@ -1,23 +1,94 @@
+// @ts-check
+
 import * as esbuild from "esbuild";
 import clipboardy from "clipboardy";
-import { readFileSync } from "fs";
+import fs from "node:fs";
+import path from "node:path";
+import { parseArgs } from "node:util";
 
-async function minifyCode(inputFile) {
-  const minifiedCode = (
-    await esbuild.transform(readFileSync(inputFile, "utf8"), {
-      minify: true,
-    })
-  ).code;
+async function minifyCode({ inputFile, outputFile, write = true }) {
+  /** @type {import('esbuild').Plugin} */
+  const bookmarkletPlugin = {
+    name: "bookmarklet",
+    setup(build) {
+      // 禁用 esbuild 的自动写入
+      build.initialOptions.write = false;
 
-  // for unknown reasons, if minified, the \n in string will be real line break
-  return minifiedCode.slice(0, -1).replaceAll("\n", "\\n");
+      build.initialOptions.minify = true;
+      build.initialOptions.supported = {
+        ...build.initialOptions.supported,
+        "template-literal": false,
+      };
+
+      build.onEnd(async ({ outputFiles }) => {
+        if (outputFiles?.length) {
+          outputFiles.forEach((file) => {
+            // 修改输出内容
+            const newContent = `javascript:(function(){${file.text.trimEnd()}})()`;
+            // 使用新的内容更新
+            file.contents = new TextEncoder().encode(newContent);
+
+            // 手动写入文件
+            if (write) {
+              fs.mkdirSync(path.dirname(file.path), { recursive: true });
+              fs.writeFileSync(file.path, file.contents);
+            }
+          });
+        }
+      });
+    },
+  };
+
+  const result = await esbuild.build({
+    entryPoints: [inputFile],
+    outfile: outputFile,
+    bundle: true,
+    plugins: [bookmarkletPlugin],
+  });
+
+  return (result.outputFiles ?? [null])[0]?.text;
 }
 
 async function build() {
-  const minified = await minifyCode("search-engine-switch.js");
-  const bookmarklet = `javascript:(function(){${minified}})()`;
-  clipboardy.writeSync(bookmarklet);
-  console.log("Bookmarklet copied to clipboard");
+  const {
+    values: { copy, inputFile, outputFile, write },
+  } = parseArgs({
+    options: {
+      copy: {
+        type: "boolean",
+        short: "c",
+      },
+      inputFile: {
+        type: "string",
+        short: "i",
+        default: "search-engine-switch.js",
+      },
+      outputFile: {
+        type: "string",
+        short: "o",
+        default: "bookmarklet",
+      },
+      write: {
+        type: "boolean",
+        short: "w",
+      },
+    },
+  });
+
+  if (!write && !copy) {
+    console.warn("No output method specified, do nothing");
+    return;
+  }
+
+  const minified = await minifyCode({ inputFile, outputFile, write });
+  if (!minified) {
+    console.error("Failed to minify code");
+    return;
+  }
+  if (copy) {
+    clipboardy.writeSync(minified);
+    console.log("Bookmarklet copied to clipboard");
+  }
 }
 
 build();
--- search-engine-switch.js
+++ search-engine-switch.js
@@ -1,67 +1,118 @@
-// 搜索引擎配置
-const searchEngines = {
-  baidu: {
-    name: "百度",
-    url: "https://www.baidu.com/s?wd=%s",
-    identifier: "baidu.com",
-    queryGetter: () => new URLSearchParams(window.location.search).get("wd"),
-  },
+// @ts-check
 
-  google: {
+/**
+ * @typedef {Object} SearchEngine
+ * @property {string} name
+ * @property {string} url
+ * @property {string} identifier
+ * @property {() => string|null} queryGetter
+ */
+
+/** @type {SearchEngine[]} */
+const searchEngines = [
+  {
     name: "Google",
     url: "https://www.google.com/search?q=%s",
     identifier: "google.com",
     queryGetter: () => new URLSearchParams(window.location.search).get("q"),
   },
-  // 必应: "https://www.bing.com/search?q=%s",
-  bing: {
+  {
+    name: "Perplexity",
+    url: "https://www.perplexity.ai/?q=%s",
+    identifier: "perplexity.ai",
+    queryGetter: () => document.querySelector("h1")?.innerText ?? null,
+  },
+  {
     name: "必应",
     url: "https://www.bing.com/search?q=%s",
     identifier: "bing.com",
     queryGetter: () => new URLSearchParams(window.location.search).get("q"),
   },
-  perplexity: {
-    name: "Perplexity",
-    url: "https://www.perplexity.ai/?q=%s",
-    identifier: "perplexity.ai",
-    queryGetter: () => document.querySelector("h1").innerText,
+  {
+    name: "百度",
+    url: "https://www.baidu.com/s?wd=%s",
+    identifier: "baidu.com",
+    queryGetter: () => new URLSearchParams(window.location.search).get("wd"),
   },
-};
+];
 
-// 获取当前搜索关键词
+/**
+ * 获取当前搜索关键词
+ * @returns {string|null}
+ */
 function getSearchQuery() {
   const url = window.location.href;
-  const engine = Object.keys(searchEngines).find((engine) =>
-    url.includes(searchEngines[engine].identifier)
+  const engine = searchEngines.find((engine) =>
+    url.includes(engine.identifier)
   );
-  const query = engine ? searchEngines[engine].queryGetter() : null;
+  if (!engine) {
+    throw new Error("未找到匹配的搜索引擎");
+  }
+  const query = engine.queryGetter();
   return query;
 }
 
-// 创建搜索引擎选择窗口
-function createEngineSelector(query) {
-  const engines = Object.values(searchEngines);
-  const selectedEngine = prompt(
-    "请选择搜索引擎(输入数字):\n" +
-      engines.map((engine, index) => `${index + 1}. ${engine.name}`).join("\n"),
-    "1"
+/**
+ * 获取用户选择的搜索引擎
+ * @returns {SearchEngine|null}
+ */
+function getSelectedEngine() {
+  const localStorageKey = "search-engine-switch-last-use";
+  
+  let lastUse = searchEngines.findIndex(
+    (engine) => engine.name === localStorage.getItem(localStorageKey)
   );
+  if (lastUse === -1) lastUse = 1;
+  else lastUse += 1;
 
-  if (
-    selectedEngine &&
-    selectedEngine > 0 &&
-    selectedEngine <= engines.length
-  ) {
-    const engine = engines[selectedEngine - 1];
-    const searchUrl = engine.url.replace("%s", encodeURIComponent(query));
-    window.open(searchUrl, "_blank");
+  const selectedEngineIndex = prompt(
+    "请选择搜索引擎(输入数字):\n" +
+      searchEngines
+        .map((engine, index) => `${index + 1}. ${engine.name}`)
+        .join("\n"),
+    lastUse.toString()
+  );
+  if (selectedEngineIndex === null) return null;
+
+  const index = parseInt(selectedEngineIndex) - 1;
+  if (isNaN(index) || !Number.isInteger(index)) {
+    throw new Error("Invalid input: please enter a valid number");
   }
+  if (index < 0 || index >= searchEngines.length) {
+    throw new Error(
+      `Please enter a number between 1 and ${searchEngines.length}`
+    );
+  }
+  const selectedEngine = searchEngines[index];
+  localStorage.setItem(localStorageKey, selectedEngine.name);
+  return selectedEngine;
+}
+
+/**
+ * 创建搜索引擎选择窗口
+ * @param {string} query
+ * @param {SearchEngine} searchEngine
+ */
+function createEngineSelector(query, searchEngine) {
+  const searchUrl = searchEngine.url.replace("%s", encodeURIComponent(query));
+  window.open(searchUrl, "_blank");
 }
 
 // 主函数
-const query = getSearchQuery();
-if (query) {
-  createEngineSelector(query);
-} else {
-  alert("未找到搜索关键词");
+function main() {
+  const query = getSearchQuery();
+  if (!query) {
+    throw new Error("未找到搜索关键词");
+  }
+  const engine = getSelectedEngine();
+  if (!engine) {
+    return;
+  }
+  createEngineSelector(query, engine);
+}
+
+try {
+  main();
+} catch (error) {
+  alert(error.message);
 }
